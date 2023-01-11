038ea04fcbbf2c4ed4a1428926db1dcb8bb82c31
Maksim_Karnitski <maksim.kornitsky@gmail.com>
2023-01-11 14:44:59 +0100

    Implement

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,24 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> result = new HashMap<>();
+        Map<Integer, String> sourceMapCopy = new HashMap<>(sourceMap);
+        Map<Integer, String> sourceMapCopyAsResult = new HashMap<>(sourceMap);
+
+        for (Map.Entry<Integer, String> s : sourceMap.entrySet()) {
+            for (Map.Entry<Integer, String> sc : sourceMapCopy.entrySet()) {
+                if (s.getValue().contains(sc.getValue()) && sc.getKey() > s.getKey()) {
+                    sourceMapCopyAsResult.remove(sc.getKey());
+                }
+            }
+        }
+        for (Map.Entry<Integer, String> s : sourceMapCopyAsResult.entrySet()) {
+            result.put(s.getValue(), s.getKey());
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,25 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> result = new HashMap<>();
+        sentence = sentence.toLowerCase();
+        List<String> words = List.of(sentence.split("\\W+"));
+        if (words.size() > 1) {
+            for (String word : words) {
+                int count = 0;
+                for (String word2 : words) {
+                    if (word.contains(word2)) {
+                        count++;
+                    }
+                }
+                result.put(word.toLowerCase(), count);
+            }
+        }
+        return result;
     }
 }
+
